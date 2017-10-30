e9efad249c1ea48f3d11cf8ff99fd83e1f8c2965
Kim Foukaris <kimfoukaris@outlook.com>
2017-10-30 07:39:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function currentLine(katzDeliLine) {
   } else {
     var line = ""
     for (let i=0; i<count; i++) {
-      line = line + " " + (i+1) + ". " + katzDeliLine[i]
+      line = line + ", " + (i+1) + ". " + katzDeliLine[i]
       }
     return "The line is currently:" + line
       }
