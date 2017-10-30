cb88819208bcb09d57d2f651091babf13aa9be63
Kim Foukaris <kimfoukaris@outlook.com>
2017-10-30 07:48:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function nowServing(katzDeliLine) {
   if (test===0) {
     return "There is nobody waiting to be served!"
   } else {
-  return "Currently serving " + katzDeliLine[0] + "."
-  katzDeliLine.shift()
+      return "Currently serving " + katzDeliLine[0] + "."
+      katzDeliLine.shift()
+      }
   return katzDeliLine
-  }
 }
 
 function currentLine(katzDeliLine) {
